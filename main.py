37a102ef4cf90bc92370608e369fae372ce4e791
Bruce Brad Fernandes <50092793+brucefernandes@users.noreply.github.com>
2022-03-23 15:34:40 -0400

    Added data corruption to app
    The function corruption() in error_sim.py changes 1 bit in each character array. This change is random

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from encodedecode import encode_text
 from encodedecode import decode_text
 from error_detection_and_correction import error_detection
 from error_detection_and_correction import error_correction
+from error_sim import corruption
 encoding = encode_text("HELLO WORLD")
-encoding[0][1] = 0
-encoding[1][1] = 0
-encoding[2][3] = 1
+# encoding[0][1] = 0
+# encoding[1][1] = 0
+# encoding[2][3] = 1
+
+corrupted_data = corruption(encoding)
+print(corrupted_data)
 detected = error_detection(encoding)
 print(detected)
 corrected = error_correction(encoding, detected)
